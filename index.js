c544d6d20614c74045aac5dd7f3a60cbe8c117c4
ChunLynda <chunlynda1@gmail.com>
2018-11-08 11:14:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
-  res.send('Hello World!\nThis is testing. Test updated.');
+  res.send('Hello World!Last updated.');
 });
 
 //Launch listening server on port 8081
